c7b42c7f7aea6323c9b84c0440bf9c2e1a8d4869
yashhguptaa1 <yashhguptaa1@gmail.com>
2020-07-06 16:58:21 +0530

    Learning ReactJS Section 2 of udemy.

--- clinicfrontend/src/index.js
+++ clinicfrontend/src/index.js
@@ -1,9 +1,48 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+// import aliasname from librarry name
 
+function getButtonText()
+{
+  return 'click on me from function';
+}
+//Create a react component
+const App=()=>{
+
+  //1.
+  //make sure the opening tag is on the same line as return
+  //otherwise it will consider return undefined or use parenthesis
+
+  //2.
+  //Outer curly braces indicates we want to reference javascript variable
+  //Inner curly braces indicate javascript object
+  //background-color in html is same as
+  //backgroundColor in JSX
+
+  //3.
+  //in html we use class in tags
+  //in jsx we use className keyword
+
+  //4. we can  directly reference javascript variables
+  //replace buttonText in curly braces withfunction getButtonText
+  const buttonText='click me from var';
+  
+  return (
+  <div>
+    <label className="label" htmlFor="name">
+      ENTER NAME:
+    </label>
+    
+    <input id="name" type="text"/>
+    <button style={{backgroundColor:'red',color:'white'}}>
+      {getButtonText()}
+    </button>
+  </div>
+  );
+
+};
+
+//Take the react component and show it on 
 ReactDOM.render(
   <React.StrictMode>
     <App />
@@ -11,7 +50,4 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ 
