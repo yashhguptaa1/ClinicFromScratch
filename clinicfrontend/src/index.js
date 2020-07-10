8656b58821621694ab79ff7c2c594b8a80758a5d
yashhguptaa1 <yashhguptaa1@gmail.com>
2020-07-10 13:39:57 +0530

    Learning to use form input to post to add new doctor ,linkedin.

--- clinicfrontend/src/index.js
+++ clinicfrontend/src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 //import HomePage from './CustomHome/homepage';
 //import CR from './CustomHome/checkOrRegCard';
 import News from './fetchaxios/News'
+import SideNews from './fetchaxios/SideNews';
+import AddDoctor from './fetchaxios/AddDoc'
 
 class App extends React.Component {
   constructor(props) {
@@ -29,9 +31,16 @@ class App extends React.Component {
         <header className="App-header">
           <h1 className="App-title">My feed</h1>
         </header>
-        <News news={this.state.doc} />
-        <News news={this.state.pat} />
-        <News news={this.state.book} />
+        <div>
+          <News news={this.state.doc} />
+          <News news={this.state.pat} />
+        </div>
+        <div>
+          <SideNews news={this.state.book}/>
+        </div>
+        <div align="center">
+          <AddDoctor/>
+        </div>
       </div>
     );
   }
